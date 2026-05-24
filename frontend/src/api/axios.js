ee551a7566e84796510897fab12949b47fadfae7
Priyanshu Kumar <kumarpriyanshu282003@gmail.com>
2026-05-25 01:24:35 +0530

    Connected frontend to deployed backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://vendor-managment-r829.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
