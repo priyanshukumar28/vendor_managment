3809a916094c915371a3105c66d378425ea46021
Priyanshu Kumar <kumarpriyanshu282003@gmail.com>
2026-05-30 01:56:11 +0530

    feat: implement business approver dashboard and approval module

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(
   cors({
     origin:[
-    process.env.production.VITE_API_URL,
+    process.env.FRONTEND_URL,
     "http://localhost:5173"
   ], 
     credentials: true,
